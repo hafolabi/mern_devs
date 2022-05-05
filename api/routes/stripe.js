51c622465dc58339dc34a089eef970633169bcb6
Afolabi Oladipo <57943516+hafolabi@users.noreply.github.com>
2022-05-05 09:34:36 +0100

    ecomm app commit

---
+++ api/routes/stripe.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const stripe = require("stripe")(
+  "sk_test_51K7N0cFdTBZ0dAku4TbC4mRL9IYzNPkgKkHAEob3BVH20IY4t9MPdRtB9sUDHsoAVJAGrNlNNKJ2dfNgZhaEsVzN00vQnqM8Y2")
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+module.exports = router;
